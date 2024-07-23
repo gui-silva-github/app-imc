36d35fc7955f9046b82f9f3eb6b1b145e8fa8ef5
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2024-07-23 19:29:29 -0300

    Adicionando estilização ao Resultado IMC

--- src/components/Form/ResultImc/style.js
+++ src/components/Form/ResultImc/style.js
@@ -3,8 +3,8 @@ import { StyleSheet } from "react-native"
 const styles = StyleSheet.create({
     resultImc: {
         flex: 1,
-        marginTop: 15,
-        paddingTop: 60,
+        marginTop: 20,
+        paddingTop: 15,
         borderRadius: 50,
         alignItems: "center",
         width: "100%",
@@ -14,10 +14,21 @@ const styles = StyleSheet.create({
         color: "#ff0043",
         fontWeight: "bold"
     },
-    information: {
-        fontSize: 18,
-        color: "#ff0043",
-        fontWeight: "bold"
+    boxShareButton: {
+        width: "100%",
+        alignItems: "center",
+        marginBottom: 10
+    },
+    shared: {
+        backgroundColor: "#1877f2",
+        borderRadius: 50,
+        paddingBottom: 5,
+        paddingTop: 5
+    },
+    sharedText: {
+        color: "#fff",
+        fontWeight: "bold",
+        paddingHorizontal: 30   
     }
 })
 
