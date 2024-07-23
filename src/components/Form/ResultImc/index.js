665f74c9c4769aca82b8cdd702d573471d54a2a8
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2024-07-23 19:29:00 -0300

    Adicionando novas funcionalidades, como o Share do resultado IMC

--- src/components/Form/ResultImc/index.js
+++ src/components/Form/ResultImc/index.js
@@ -1,12 +1,28 @@
 import React from "react"
-import { View, Text } from "react-native"
+import { View, Text, TouchableOpacity, Share } from "react-native"
 import styles from "./style"
 
 export default function ResultImc(props){
+
+    const onShare = async ()=>{
+
+        const result = await Share.share({
+            message: "Meu IMC hoje é: " + props.resultImc
+        })
+
+    }
+
     return(
         <View style={styles.resultImc}>
-            <Text style={styles.information}>{props.messageResultImc}</Text>
-            <Text style={styles.numberImc}>{props.resultImc}</Text>
+            <View style={styles.boxShareButton}>
+                    <Text style={styles.information}>{props.messageResultImc}</Text>
+                    <Text style={styles.numberImc}>{props.resultImc}</Text>
+                    <TouchableOpacity
+                    style={styles.shared}
+                    onPress={onShare}>
+                        <Text style={styles.sharedText}>Share</Text>
+                    </TouchableOpacity>
+            </View>
         </View>
     )
 }
